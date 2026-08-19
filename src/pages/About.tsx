9b3b635cad08bbeff35f39328dbb3bbff1fc4079
C.J. Wade <sudge64@icloud.com>
2026-08-19 17:55:47 -0500

    Update: Gallery text.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,54 +3,45 @@ const About = () => {
     <div className="text-background">
       <div style={{ padding: "20px" }}>
         <h1>About</h1>
-        <h3>Hope Streams</h3>
+        <h3>Meet The Maker</h3>
         <p>
-          Founded in 2008, Hope Streams is a family-owned and operated business
-          dedicated to producing products which assist people of all ages in
-          connecting to The Perfect Parent (the One who makes us healed and
-          whole).
+           One seemingly ordinary day, I decided to check out my new neighbor’s garage sale, and in a matter of minutes, I found myself in his knife-making shop engrossed in a very interesting conversation. That day lit a spark and I discovered a passion for knife-making, and have been making them now for almost a decade.
         </p>
+        <br/>
         <p>
-          Many of our products center around empowering parents (and caregivers)
-          to love and nurture their children’s spirits so they can know and
-          experience God in their hearts, not just their heads. The good news is
-          that it’s never too late to start growing the spirit so EACH person
-          (parent, child, person) can carry their own portable abundant life
-          that the Lord Jesus Christ paid the ultimate price for!
+          It didn’t take long, like many other knife-makers, to realize my knives needed sheaths. Leather runs in my blood, so to speak. My great-grandmother worked, sewing leather stirrups, in a saddle shop, and my dad worked in leather work in his early teens. With scrap leather and my dad’s old tools, long story short, JCW Maker Custom Goods was born.
         </p>
-        <h3>Crystal Wade</h3>
+        <br/>
         <p>
-          Crystal Wade writes to throw out a lifeline of hope for people to
-          receive connectivity to trust the Father and be internally aligned to
-          relate to Him, and thus receive His relationship and the blessings
-          that naturally flow from Him. She loves to discover the redemptive
-          plan of the Father in people, places, and things. She co-authored Pure
-          Joy and wrote Perfect Peace together with its audio album, tools for
-          healing trust and growing the spirit. She is managing editor of Hope
-          Streams.net. Crystal and her husband Stephen share a passion to
-          position the next generation to be anchored in truth so they can fly
-          in the spirit. To that end, their family launched Let’s Wade In, a
-          media portal, which is both a community for children and teens and a
-          tool for parents and caregivers to anchor students in truth so they
-          can fly in the Spirit. They have three wonderful sons.
+          I’m a seventh-generation Texan – and more importantly, I’m a son of God through Jesus my Savior, who has bestowed in me the gifts to do the work that I do. I love making something with my hands from the gifts He gave me that will work – or play -- as hard as the people who use them.
         </p>
-        <h3>Stephen Wade</h3>
+        <br/>
         <p>
-          Stephen Wade grew up in Texas, but has traveled throughout North
-          America in his work in insurance. Having served as a youth pastor, he
-          holds a deep desire to see the next generations anchored in truth and
-          love, walking in freedom. Stephen sees the beauty in Creation and
-          captures it on film; he also possesses an innate ability to perceive
-          the hidden value in aged and broken furniture, machinery, etc., and
-          under his careful attention, restoration is worked.
+          If you’ll notice, pricing in my Gallery uses the phrase “starting at.”
         </p>
-        <h3>Our Purpose</h3>
+        <br/>
         <p>
-          Stephen and Crystal received a commission from the Father to connect
-          the generations to Him and to each other so that they can: Start well.
-          Live well, Recover well, Build well, and Finish well. As part of that
-          commission, they release materials to assist people of all ages in
-          connecting to The Perfect Parent.
+          Every piece I create is special, and almost every one is custom. With custom work, sizes vary – which affect the amount of leather needed. Some folks prefer tooling, while others select plain. Finished edges truly elevate any leather piece, and finished edges add time to any project. Sizing, material, details like edging or painting, supply chain issues and inflation fluctuations all impact price.
+        </p>
+        <br/>
+        <p>
+          I primarily take custom orders and I’d love to make one for you. I use the top-quality leather and materials that I can get my hands on. My Gallery shows a portfolio of some products I’ve made.
+        </p>
+        <br/>
+        <ul>
+          <li>knife sheaths</li>
+          <li>gun leathers</li>
+          <li>belts</li>
+          <li>wallets</li>
+          <li>bags, etc.</li>
+        </ul>
+        <br/>
+        <p>
+          Thanks for stopping by! You can reach me via my Contact page or social media.
+        </p>
+        <br/>
+        <p>
+          All the best to you and yours.
         </p>
       </div>
     </div>
